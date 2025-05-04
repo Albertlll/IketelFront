0dff9a27730d04bd5267666a020886927dfe30fa
Alan <king.python@mail.ru>
2025-05-05 00:47:27 +0300

    эндпоинт поменял

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useUserStore } from "./entities/user/model/store";
 
 const httpConfig = axios.create({
-	baseURL: "http://localhost:8000",
+	baseURL: "https://iketel.ru/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
