16f1bb8ebf29969cdb4ff9befd6848998eae46c8
Alan <king.python@mail.ru>
2025-05-13 22:52:10 +0300

    лидерборд и прохождение работают

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -2,29 +2,29 @@ import axios from "axios";
 import { useUserStore } from "./entities/user/model/store";
 
 const httpConfig = axios.create({
-  // baseURL: "https://iketel.ru/api",
-  baseURL: "http://localhost:8000/",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: "https://iketel.ru/api",
+	// baseURL: "http://localhost:8000/",
+	withCredentials: true,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 httpConfig.interceptors.request.use((config) => {
-  const token = useUserStore.getState().token;
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+	const token = useUserStore.getState().token;
+	if (token) config.headers.Authorization = `Bearer ${token}`;
+	return config;
 });
 
 httpConfig.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      useUserStore.getState().clearUser();
-      window.location.href = "/auth";
-    }
-    return Promise.reject(error);
-  },
+	(response) => response,
+	(error) => {
+		if (error.response?.status === 401) {
+			useUserStore.getState().clearUser();
+			window.location.href = "/auth";
+		}
+		return Promise.reject(error);
+	},
 );
 
 export default httpConfig;
