baedbfb2b8f318dbca0a3eec2436571450b5077b
Alan <king.python@mail.ru>
2025-05-14 06:09:02 +0300

    для тс

--- src/sockets.ts
+++ src/sockets.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 import { useUserStore } from "./entities/user/model/store";
 
-// const SOCKET_URL = "ws://localhost:8000";
-const SOCKET_URL = "wss://iketel.ru";
+const SOCKET_URL = "ws://localhost:8000";
+// const SOCKET_URL = "wss://iketel.ru";
 
 // Создаем инстанс
 const socket = io(SOCKET_URL, {
